d260d215b81dcf69e588bb6f5adbe20e2b3d8fc3
Hossein Nassiri <hosseinnassiri2002@hotmail.com>
2022-01-23 16:33:29 -0500

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  //const loggerService = app.get(Logger);
+
+  // SWAGGER
+  const optionsSwagger = new DocumentBuilder()
+    .setTitle('NestJS Typescript Boilerplate')
+    .setDescription('The NestJS API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, optionsSwagger);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
